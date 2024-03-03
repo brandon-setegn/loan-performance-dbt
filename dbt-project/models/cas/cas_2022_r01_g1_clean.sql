a55e40b01128f14c75fca5d9e2c8d42387a8a7c3
brandon-setegn <bjsetegn@gmail.com>
2024-03-02 20:51:44 -0500

    Add delinquency status calculations and metrics

--- dbt-project/models/cas/cas_2022_r01_g1_clean.sql
+++ dbt-project/models/cas/cas_2022_r01_g1_clean.sql
@@ -9,8 +9,8 @@
 WITH with_period AS (
     SELECT
         PARSE_DATE('%m%Y', monthly_reporting_period) AS reporting_period
-        
-        ,*
+        ,CAST(current_loan_delinquency_status AS INT64) as current_loan_delinquency_status
+        ,* EXCEPT(current_loan_delinquency_status)
     FROM
         {{ source('cas_loanperf', 'cas_2022_r01_g1')}}
 )
@@ -26,6 +26,9 @@ SELECT
     ,SUM(total_principal_current) OVER (PARTITION BY loan_identifier ORDER BY reporting_period
         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
     ) AS total_principal_cumulative
+    ,IF(current_loan_delinquency_status >= 2, 1, 0) as dq60plus
+    ,IF(current_loan_delinquency_status >= 3, 1, 0) as dq90plus
+    ,IF(current_loan_delinquency_status >= 4, 1, 0) as dq120plus
     ,* EXCEPT (reporting_period)
 FROM
     with_period
