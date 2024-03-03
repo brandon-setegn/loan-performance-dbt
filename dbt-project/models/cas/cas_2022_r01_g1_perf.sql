01b3994f4009a8949720ca6c1d9009788c1765f6
brandon-setegn <bjsetegn@gmail.com>
2024-03-02 20:32:07 -0500

    Add additional aggregations to CAS 2022 R01 G1 performance SQL

--- dbt-project/models/cas/cas_2022_r01_g1_perf.sql
+++ dbt-project/models/cas/cas_2022_r01_g1_perf.sql
@@ -22,7 +22,9 @@ SELECT
   sum(total_deferral_amount) as total_deferral_amount,
   sum(interest_bearing_upb) as interest_bearing_upb,
   sum(principal_forgiveness_amount) as principal_forgiveness_amount,
-  sum(modification_related_non_interest_bearing_upb) as modification_related_non_interest_bearing_upb
+  sum(modification_related_non_interest_bearing_upb) as modification_related_non_interest_bearing_upb,
+  sum(current_period_modification_loss_amount) as current_period_modification_loss_amount,
+  sum(delinquent_accrued_interest) as delinquent_accrued_interest
 FROM
     {{ ref("cas_2022_r01_g1_clean")}}
 GROUP BY
